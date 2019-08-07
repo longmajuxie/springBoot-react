caa40226f7abc1eb07367c9abb2bc0f0770e0735
longmajuxie <897114723@qq.com>
2019-08-08 01:49:10 +0800

    first commit

---
+++ backend/src/main/java/com/spring/Demo.java
@@ -0,0 +1,11 @@
+package com.spring;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class Demo {
+    public static void main(String[] args){
+        SpringApplication.run(Demo.class,args);
+    }
+}
